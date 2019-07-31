72c8117ca943fa52612d0af97e1eba3a941408be
eperez1351 <50218290+eperez1351@users.noreply.github.com>
2019-07-31 16:53:24 -0400

    minunit terminado
    solo un error en una de las autopruebas queda pendiente

--- minunit_CasparCGclient-in-C-GTK/main.c
+++ minunit_CasparCGclient-in-C-GTK/main.c
@@ -3,24 +3,32 @@
 
 #define MINUNIT_EPSILON 1E-12
 
-char play[]
-char stop[]
-char pause[]
-char resume[]
-char
+char play[] = "play 1-1 file\r\n";
+char stop[] = "stop 1-1\r\n";
+char pausestring[] = "pause 1-1\r\n";
+char resume[] = "resume 1-1\r\n";
+char clear[] = "clear 1\r\n";
+char print[] = "print 1\r\n";
+char cls[] = "cls\r\n";
+char setlogo[] = "play 1-2 file\r\n";
+
+char validFilename[] = "1";
+char invalidFilename[] = "2";
 
 
 
 MU_TEST(test_checkCommandFunctions) {
-	mu_assert_string_eq("Thisstring", foostring);
-	mu_assert_string_eq("Thisstring", foostring);
-	mu_assert_string_eq("Thisstring", foostring);
-	mu_assert_string_eq("Thisstring", foostring);
-	mu_assert_string_eq("Thisstring", foostring);
-	mu_assert_string_eq("Thisstring", foostring);
-	mu_assert_string_eq("Thisstring", foostring);
-	mu_assert_string_eq("Thisstring", foostring);
-	mu_assert_string_eq("Thisstring", foostring);
+	mu_assert_string_eq(playCommand("file"), play);
+	mu_assert_string_eq(stopCommand(), stop);
+	mu_assert_string_eq(pauseCommand(), pausestring);
+	mu_assert_string_eq(resumeCommand(), resume);
+	mu_assert_string_eq(clearCommand("1"), clear);
+	mu_assert_string_eq(printCommand("1"), print);
+	mu_assert_string_eq(clsCommand(), cls);
+	mu_assert_string_eq(setLogo("file"), setlogo);
+	mu_assert_string_eq(readCommandsFromFile(validFilename), "play 1-1 amb\r\n");
+    mu_assert_string_eq(readCommandsFromFile(invalidFilename), "0");
+
 
 
 }
